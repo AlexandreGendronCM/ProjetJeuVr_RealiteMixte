baea133d14fed9fa2f72fe52a8335108fea98ffc
MikaelArseneau <mikael.arseneau@hotmail.ca>
2025-05-04 22:24:37 -0400

    les cranes bougent :)

--- Chasseur de cranes/Assets/attaque.cs
+++ Chasseur de cranes/Assets/attaque.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class attaque : MonoBehaviour
 {
     // Start is called before the first frame update
+    private int counter;
+    public int cranes;
+    public TextMeshProUGUI score;
     void Start()
     {
         
@@ -13,7 +17,8 @@ public class attaque : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+         int currentScore = cranes - counter;
+    score.text = "Score: " + currentScore;
     }
 
   
@@ -22,6 +27,7 @@ public class attaque : MonoBehaviour
         if (other.tag == "epee") {
             gameObject.SetActive(false);
             Debug.Log("test");
+            counter++;
         }
     }
 }
