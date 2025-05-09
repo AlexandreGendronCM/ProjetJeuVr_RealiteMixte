b293061c17e5bb462e21315c5c9e058f7310f1c2
mathieuwillett <143769896+mathieuwillett@users.noreply.github.com>
2025-05-08 21:01:26 -0400

    vie solved, bug attaque

--- Chasseur de cranes/Assets/attaque2.cs
+++ Chasseur de cranes/Assets/attaque2.cs
@@ -1,11 +1,11 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class attaque : MonoBehaviour
+public class attaque2 : MonoBehaviour
 {
-    // Variables publiques
+   
     public int counter;
     public int cranes;
     public TextMeshProUGUI score;
@@ -14,34 +14,34 @@ public class attaque : MonoBehaviour
     public GameObject joueur;
     public AudioSource rate;
     public AudioSource reussi;
-    // Seuil de v�locit� minimum pour que l'�p�e active la d�sactivation
-    public float seuilVitesse = 1f;  // Par exemple, 1 unit� par seconde
+    
+    public float seuilVitesse = 1f;  
 
-    // Variables priv�es pour suivre la position pr�c�dente
+  
     private Vector2 previousPosition;
     private Vector3 previousPosition2;
     private float velocity;
     private float velocity2;
     void Start()
     {
-        // Initialiser la position pr�c�dente
+       
         previousPosition = epee.transform.position;
         previousPosition2 = epee2.transform.position;
     }
 
     void Update()
     {
-        // Calculer la v�locit� de l'�p�e � partir de la position pr�c�dente
+        
         Vector2 currentPosition = epee.transform.position;
         Vector3 currentPosition2 = epee2.transform.position;
         velocity = (currentPosition - previousPosition).magnitude / Time.deltaTime;
         velocity2 = (currentPosition2 - previousPosition2).magnitude / Time.deltaTime;
-        // Mettre � jour la position pr�c�dente pour le prochain calcul
+       
         previousPosition = currentPosition;
         previousPosition2 = currentPosition2;
 
 
-       // Debug.Log(counter);
+       
         int currentScore = cranes - counter;
         score.text = "Score: " + currentScore;
     }
@@ -50,19 +50,19 @@ public class attaque : MonoBehaviour
     {
         if (other.tag == "epee")
         {
-            // V�rifie si la v�locit� d�passe le seuil
-            if (velocity >= seuilVitesse || velocity2 >= seuilVitesse)  // Si la vitesse est sup�rieure ou �gale au seuil
+            
+            if (velocity >= seuilVitesse || velocity2 >= seuilVitesse)  
             {
-                // Si la v�locit� est suffisante, d�sactive l'objet
+                
                 gameObject.SetActive(false);
-                Debug.Log("Audio lanc�");
+                Debug.Log("Audio lance");
                 reussi.Play();
                 counter++;
                 Debug.Log("Compteur: " + counter);
             }
             else
             {
-                Debug.Log("Audio lanc�");
+                Debug.Log("Audio lance");
                 rate.Play();
             }
         }
--- Chasseur de cranes/Assets/vies.cs
+++ Chasseur de cranes/Assets/vies.cs
@@ -1,53 +1,64 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class vies : MonoBehaviour
 {
     public int vie = 3;
-    public int compteur = 0;
-    public GameObject epee;
-    public GameObject epee2;
-    // Start is called before the first frame update
-    private Vector2 previousPosition;
-    private Vector3 previousPosition2;
-    private float velocity;
-    private float velocity2;
-    public int inactiveCount = 0;
+    public float invincibilityDuration = 1.5f;
+    private bool isInvincible = false;
+
+    public Image damageFlash;
+    public AudioSource hitSound;
+    public Slider lifeSlider; 
+
     void Start()
     {
-        Debug.Log(vie);
-        previousPosition = epee.transform.position;
-        previousPosition2 = epee2.transform.position;
+        // Set slider max and current value
+        if (lifeSlider != null)
+        {
+            lifeSlider.maxValue = vie;
+            lifeSlider.value = vie;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnCollisionEnter(Collision other)
     {
-        
-
-       
+        if (other.gameObject.CompareTag("crane") && !isInvincible)
+        {
+            StartCoroutine(LoseLife());
+        }
     }
-    public void OnTriggerEnter(Collider other)
-    {
-         
-        
-         if (other.tag == "crane") {
-            if (vie > 0) {
-                vie--;
-                Debug.Log(vie);
-            }else if(vie <=0)
-            {
-                Debug.Log("terminus");
-                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-            }
 
+    IEnumerator LoseLife()
+    {
+        vie--;
+        Debug.Log("Collision with crane! Lives left: " + vie);
+
+        if (lifeSlider != null)
+            lifeSlider.value = vie;
+
+        if (hitSound != null)
+            hitSound.Play();
+
+        if (damageFlash != null)
+        {
+            damageFlash.color = new Color(1f, 0f, 0f, 0.5f);
+            yield return new WaitForSeconds(1f);
+            damageFlash.color = new Color(1f, 0f, 0f, 0f);
         }
 
-
+        if (vie <= 0)
+        {
+            Debug.Log("Game Over - Restarting Scene");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        else
+        {
+            isInvincible = true;
+            yield return new WaitForSeconds(invincibilityDuration);
+            isInvincible = false;
+        }
     }
-
-    
-    
 }
