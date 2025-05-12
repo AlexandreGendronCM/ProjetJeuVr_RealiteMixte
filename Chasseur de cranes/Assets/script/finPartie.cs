cbc7c2f072fc2318d3b6c532bf9042cb7056532b
mathieuwillett <143769896+mathieuwillett@users.noreply.github.com>
2025-05-11 22:54:21 -0400

    osc done

--- Chasseur de cranes/Assets/script/finPartie.cs
+++ Chasseur de cranes/Assets/script/finPartie.cs
@@ -2,13 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 public class finPartie : MonoBehaviour
 {
+    public Button rejouer_bouton;
+    public Button quitter_bouton;
+    public Button quitter_spawn;
+    public GameObject canvas_fin;
     // Start is called before the first frame update
     void Start()
     {
-        
+         rejouer_bouton.onClick.AddListener(rejouer);
+         quitter_bouton.onClick.AddListener(quitter);
+         quitter_spawn.onClick.AddListener(quitter);
     }
 
     // Update is called once per frame
@@ -17,9 +25,22 @@ public class finPartie : MonoBehaviour
         
     }
 
-    public void RestartScene()
+    void RestartScene()
     {
-        Debug.Log("Scene is restarting...");
+        canvas_fin.SetActive(true);
+    }
+
+    void rejouer()
+    {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    void quitter()
+    {
+        Application.Quit();
+
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #endif
     }
 }
---
+++ Chasseur de cranes/Assets/script/osc_script.cs
@@ -0,0 +1,190 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using extOSC;
+
+
+public class osc_script : MonoBehaviour
+{
+    public extOSC.OSCReceiver ReceveurOSC;
+    public extOSC.OSCTransmitter TransmetteurOSC;
+
+
+    public Light lumiereTorche1;
+    public Light lumiereTorche2;
+    public Light lumiereTorche3;
+    public Light lumiereTorche4;
+    public Light lumiereTorche5;
+    public Light lumiereTorche6;
+    public Light lumiereTorche7;
+    public Light lumiereTorche8;
+    public Light lumiereTorche9;
+    public Light lumiereTorche10;
+    public Light lumiereTorche11;
+
+    public UnityEngine.AI.NavMeshAgent crane1;
+    public UnityEngine.AI.NavMeshAgent crane2;
+    public UnityEngine.AI.NavMeshAgent crane3;
+    public UnityEngine.AI.NavMeshAgent crane4;
+    public UnityEngine.AI.NavMeshAgent crane5;
+    public UnityEngine.AI.NavMeshAgent crane6;
+    public UnityEngine.AI.NavMeshAgent crane7;
+    public UnityEngine.AI.NavMeshAgent crane8;
+    public UnityEngine.AI.NavMeshAgent crane9;
+    public UnityEngine.AI.NavMeshAgent crane10;
+
+    private void Start()
+    {
+        ReceveurOSC.Bind("*", TraiterMessageOSC);
+
+    }
+
+    public void TestOSC (OSCMessage oscMessage){
+        Debug.Log(oscMessage);
+
+
+    }
+
+    void TraiterMessageOSC(OSCMessage oscMessage)
+    {
+        // R�cup�rer une valeur num�rique en tant que float
+        // m�me si elle est de type float ou int :
+        float valueLumiere;
+        float valueVitesse;
+
+
+
+        if (oscMessage.Values[0].Type == OSCValueType.Int)
+        {
+            valueLumiere = oscMessage.Values[0].IntValue;
+
+        }
+        else if (oscMessage.Values[0].Type == OSCValueType.Float)
+        {
+            valueLumiere = oscMessage.Values[0].FloatValue;
+            valueVitesse = oscMessage.Values[0].FloatValue;
+
+             if (oscMessage.Address == "/lumiere")
+            {
+
+                if (lumiereTorche1 != null)
+                {
+                    lumiereTorche1.intensity = valueLumiere;
+                }
+
+                if (lumiereTorche2 != null)
+                {
+                    lumiereTorche2.intensity = valueLumiere;
+                }
+
+                if (lumiereTorche3 != null)
+                {
+                    lumiereTorche3.intensity = valueLumiere;
+                }
+
+                if (lumiereTorche4 != null)
+                {
+                    lumiereTorche4.intensity = valueLumiere;
+                }
+
+                if (lumiereTorche5 != null)
+                {
+                    lumiereTorche5.intensity = valueLumiere;
+                }
+
+                if (lumiereTorche6 != null)
+                {
+                    lumiereTorche6.intensity = valueLumiere;
+                }
+
+                if (lumiereTorche7 != null)
+                {
+                    lumiereTorche7.intensity = valueLumiere;
+                }
+
+                if (lumiereTorche8 != null)
+                {
+                    lumiereTorche8.intensity = valueLumiere;
+                }
+
+                if (lumiereTorche9 != null)
+                {
+                    lumiereTorche9.intensity = valueLumiere;
+                }
+
+                if (lumiereTorche10 != null)
+                {
+                    lumiereTorche10.intensity = valueLumiere;
+                }
+
+                if (lumiereTorche11 != null)
+                {
+                    lumiereTorche11.intensity = valueLumiere;
+                }
+
+            }
+
+
+            if (oscMessage.Address == "/vitesse")
+            {
+                if (crane1 != null)
+                {
+                    crane1.speed = valueVitesse;
+                }
+
+                if (crane2 != null)
+                {
+                    crane2.speed = valueVitesse;
+                }
+
+                if (crane3 != null)
+                {
+                    crane3.speed = valueVitesse;
+                }
+
+                if (crane4 != null)
+                {
+                    crane4.speed = valueVitesse;
+                }
+
+                if (crane5 != null)
+                {
+                    crane5.speed = valueVitesse;
+                }
+
+                if (crane6 != null)
+                {
+                    crane6.speed = valueVitesse;
+                }
+
+                if (crane7 != null)
+                {
+                    crane7.speed = valueVitesse;
+                }
+
+                if (crane8 != null)
+                {
+                    crane8.speed = valueVitesse;
+                }
+
+                if (crane9 != null)
+                {
+                    crane9.speed = valueVitesse;
+                }
+
+                if (crane10 != null)
+                {
+                    crane10.speed = valueVitesse;
+                }
+            }
+        }
+        else
+        {
+            // Si la valeur n'est ni un foat ou int, on quitte la m�thode :
+            return;
+        }
+
+
+    }
+
+}
